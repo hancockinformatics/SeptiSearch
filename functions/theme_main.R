bb27f1702d2943cb6399263b6aa8b8271d4265c3
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-10-21 13:32:38 -0700

    Added custom function for plotting theme.

---
+++ functions/theme_main.R
@@ -0,0 +1,26 @@
+#' theme_main
+#'
+#' @param baseSize Base font size for the theme
+#' @param baseFamily Base font family
+#'
+#' @return Theme object
+#'
+#' @export
+#'
+#' @import ggplot2
+#'
+theme_main <- function(baseSize = 16, baseFamily = "") {
+
+  theme_light(base_size = baseSize, base_family = baseFamily) +
+    theme(
+      text             = element_text(colour = "black"),
+      axis.text        = element_text(colour = "black"),
+      axis.ticks       = element_line(colour = "black", size = 0.5),
+      panel.grid.major = element_line(colour = "grey", size = 0.5),
+      panel.grid.minor = element_blank(),
+      panel.border     = element_rect(colour = "black", size = 1),
+      strip.background = element_rect(fill = NA),
+      strip.text       = element_text(colour = "black", face = "bold", size = 14)
+    )
+
+}
