fac81f6311cefe457baf2bf38ad54e6cb0f19226
Travis <travis.m.blimkie@gmail.com>
2022-07-28 10:08:55 -0700

    Adjusted formatting of JS code used to lazy load packages.

--- www/js/client.js
+++ www/js/client.js
@@ -1,19 +1,18 @@
 const handlers = {
   lazyLoadPackages: num => {
-    // First argument is input name
-    // Second argument is value to send
+    // First argument is input name, second argument is value to send
     Shiny.onInputChange("sessionInitialized", num);
   },
 
   initGetStarted: () => {
     const getStartedButton = document.getElementById("get_started");
-    const learnMoreButton = document.getElementById("learn_more")
+    const learnMoreButton = document.getElementById("learn_more");
 
     getStartedButton.innerHTML = "Get started";
     getStartedButton.title = "Click here to start using SeptiSearch";
     getStartedButton.classList.remove("disabled");
 
-    learnMoreButton.classList.remove("btn-hidden")
+    learnMoreButton.classList.remove("btn-hidden");
   }
 };
 
